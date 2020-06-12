11284487d7ef61513acaa232e451890fc77a4d15
beyzayldrm <beyza.yildirim0@gmail.com>
2020-06-11 21:12:17 -0400

    Test class getters and setters added

--- Covid19Testing-Backend/src/main/java/covid19testing/Test.java
+++ Covid19Testing-Backend/src/main/java/covid19testing/Test.java
@@ -2,8 +2,51 @@ package covid19testing;
   
 public class Test{
 
-        public static void main(String[] args){
-}
+	private boolean result;
+
+        public boolean getResult() {
+                return this.result;
+        }
+        public boolean setResult(boolean result){
+                this.result = result;
+        }
+	
+	private int fever;
+
+        public int getFever() {
+                return this.fever;
+        }
+        public void setFever(int Fever){
+                this.fever = fever;
+        }
+
+	private Status status;
+
+        public Status getStatus() {
+                return this.status;
+        }
+        public void setStatus(Status status){
+                this.status = status;
+        }
+
+	private Date date;
+
+        public Date getDate() {
+                return this.date;
+        }
+        public void setDate(Date date){
+                this.date = date;
+        }
+	
+	private Time time;
+
+        public Time getTime() {
+                return this.time;
+        }
+        public void setTime(Time time){
+                this.time = time;
+        }
+	
 //j'sais pas honestly
 
 }
