9e3746d2a239794ee1a2698a8131793eee8df291
beyzayldrm <beyza.yildirim0@gmail.com>
2020-06-11 21:15:54 -0400

    TestCenter class getters and setters added

--- Covid19Testing-Backend/src/main/java/covid19testing/TestCenter.java
+++ Covid19Testing-Backend/src/main/java/covid19testing/TestCenter.java
@@ -2,8 +2,42 @@ package covid19testing;
   
 public class TestCenter{
 
-        public static void main(String[] args){
-}
+        private String name;
+
+        public String getName() {
+                return this.name;
+        }
+        public void setName(String name){
+                this.name = name;
+        }
+
+        private String address;
+
+        public String getAddress() {
+                return this.address;
+        }
+        public void setAddress(String addres){
+                this.adress = adress;
+        }
+
+        private String city;
+
+        public String getCity() {
+                return this.city;
+        }
+        public void setCity(String city){
+                this.City = city;
+        }
+
+        private String province;
+
+        public String getProvince() {
+                return this.province;
+        }
+        public void setProvince(String province){
+                this.province = province;
+        }
+
         public int proximityToPatient();
         public String rankTestCenter();
         public void selectTestCenter();
