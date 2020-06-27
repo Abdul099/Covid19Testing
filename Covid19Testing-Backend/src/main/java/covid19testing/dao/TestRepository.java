cce799eec946d09620376b88eb4b6040c4d2c319
beyzayldrm <beyza.yildirim0@gmail.com>
2020-06-26 21:16:13 -0400

    dao test finished

--- Covid19Testing-Backend/src/main/java/covid19testing/dao/TestRepository.java
+++ Covid19Testing-Backend/src/main/java/covid19testing/dao/TestRepository.java
@@ -1,4 +1,12 @@
 package covid19testing.dao;
 
+import covid19testing.model.Test;
+import org.springframework.data.repository.CrudRepository;
+
 public interface TestRepository {
+
+	Test findTestByTestID(int testID)
+
+	void deleteTestByTestID(int testID)
+
 }
