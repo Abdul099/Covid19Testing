842e077d175c5a4407623cb8aa6f5ab40b24ee8b
beyzayldrm <beyza.yildirim0@gmail.com>
2020-06-28 11:51:56 -0400

    TestDto added

---
+++ Covid19Testing-Backend/src/main/java/covid19testing/dto/TestDto.java
@@ -0,0 +1,76 @@
+package covid19testing.dto;
+
+import covid19testing.model.*;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+public class TestDto {
+
+    	private int fever;
+    	private Date date;
+    	private Time time;
+    	private String patientInsuranceNo;
+    	private String testCenterName;
+    	private String testID;
+
+	public TestDto(){
+	}
+
+	public TestDto(int fever, Date date, Time time, String patientInsuranceNo, String testCenterName, String testID){
+	this.fever = fever;
+	this date = date;
+	this time = time;
+	this patientInsuranceNo = patientInsuranceNo;
+	this testCenterName = testCenterName;
+	this testID = testID;
+}
+
+    public int getFever() {
+        return this.fever;
+    }
+
+    public void setFever(int Fever) {
+        this.fever = fever;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return this.time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public String getPatientInsuranceNo() {
+        return patientInsuranceNo;
+    }
+
+    public void setPatientInsuranceNo(String patientInsuranceNo) {
+        this.patientInsuranceNo = patientInsuranceNo;
+    }
+
+    public String getTestCenterName() {
+        return testCenterName;
+    }
+
+    public void setTestCenterName(String testCenterName) {
+        this.testCenterName = testCenterName;
+    }
+
+    public String getTestID() {
+        return testID;
+    }
+
+    public void setTestID(String testID) {
+        this.testID = testID;
+    }
+}	
